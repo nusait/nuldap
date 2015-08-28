2743d44629480a2f70b7d8d3eb73edd04b548938
Vahid Azam <vahidazam@gmail.com>
2015-08-28 10:21:22 -0500

    updated coding to PSR-2, added searchField function

--- src/Nusait/Nuldap/NuLdap.php
+++ src/Nusait/Nuldap/NuLdap.php
@@ -1,39 +1,88 @@
 <?php namespace Nusait\Nuldap;
 
-class NuLdap {
+class NuLdap
+{
     protected $rdn;
     protected $password;
     protected $host;
     protected $port;
-    public function __construct($rdn = null, $password = null, $host = 'ldaps://registry.northwestern.edu/', $port = 636) {
-	$this->rdn = $rdn;
-	$this->password = $password;
-	$this->host = $host;
-	$this->port = $port;
-	return $this;
+
+    public function __construct(
+        $rdn = null,
+        $password = null,
+        $host = 'ldaps://registry.northwestern.edu/',
+        $port = 636
+    ) {
+        $this->rdn = $rdn;
+        $this->password = $password;
+        $this->host = $host;
+        $this->port = $port;
+
+        return $this;
     }
-    public function connect() {
+
+    public function connect()
+    {
         $resource = ldap_connect($this->host, $this->port) or die("No connect $this->host");
+
         return $resource;
     }
-    public function validate($netid, $password) {
-	$connection = $this->connect();
-	$rdn = "uid={$netid},ou=people,dc=northwestern,dc=edu";
+
+    public function validate($netid, $password)
+    {
+        $connection = $this->connect();
+        $rdn = "uid={$netid},ou=people,dc=northwestern,dc=edu";
         $bind = @ldap_bind($connection, $rdn, $password);
-	return (bool) $bind;
+
+        return (bool)$bind;
     }
-    public function searchNetid($netid) {
-	$connection = $this->connect();
-	$bind = ldap_bind($connection, $this->rdn, $this->password);
-	$search = ldap_search($connection, 'dc=northwestern,dc=edu', "(nuIdTag={$netid})");
-	$entries = ldap_get_entries($connection, $search);
-	$data = $entries[0];
-	return $data;	
+
+    public function searchField($field, $value)
+    {
+        $results = null;
+        $field = trim(strtolower($field));
+        $connection = $this->connect();
+        $bind = ldap_bind($connection, $this->rdn, $this->password);
+        if ($field == 'netid') {
+            $searchTerm = "(nuidtag={$value})";
+        } elseif ($field == 'email') {
+            $searchTerm = "(mail={$value})";
+        } elseif ($field == 'emplid') {
+            $searchTerm = "(employeenumber={$value})";
+        } elseif ($field == 'studentid') {
+            $searchTerm = "(nustudentnumber={$value})";
+        } else {
+            $searchTerm = null;
+        }
+        if (! is_null($searchTerm)) {
+            $search = ldap_search($connection, 'dc=northwestern,dc=edu', $searchTerm);
+            $entries = ldap_get_entries($connection, $search);
+            if ($entries['count'] != 0) {
+                $results = $entries[0];
+            }
+        }
+
+        return $results;
     }
-    public function setPassword($password) {
-	$this->password = $password;
+
+    public function searchNetid($netid)
+    {
+        $connection = $this->connect();
+        $bind = ldap_bind($connection, $this->rdn, $this->password);
+        $search = ldap_search($connection, 'dc=northwestern,dc=edu', "(nuIdTag={$netid})");
+        $entries = ldap_get_entries($connection, $search);
+        $data = $entries[0];
+
+        return $data;
     }
-    public function setRdn($rdn) {
-	$this->rdn = $rdn;
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setRdn($rdn)
+    {
+        $this->rdn = $rdn;
     }
 }
