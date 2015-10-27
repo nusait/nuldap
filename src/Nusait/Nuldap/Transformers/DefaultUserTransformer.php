abdb886e60a0e6f6567ebf603db669ce8a08b77b
azamtav <vahidazam@gmail.com>
2015-10-27 15:09:37 -0500

    added transformers

---
+++ src/Nusait/Nuldap/Transformers/DefaultUserTransformer.php
@@ -0,0 +1,19 @@
+<?php namespace Nusait\Nuldap\Transformers;
+
+class DefaultUserTransformer extends AbstractTransformer
+{
+    public function transform($ldapUser)
+    {
+        return [
+            'netid'       => $this->getSetValueOrNull($ldapUser, 'uidtag'),
+            'phone'       => $this->getSetValueOrNull($ldapUser, 'telephonenumber'),
+            'email'       => $this->getSetValueOrNull($ldapUser, 'mail'),
+            'title'       => $this->getSetValueOrNull($ldapUser, 'title'),
+            'first_name'  => $this->getSetValueOrNull($ldapUser, 'givenname'),
+            'last_name'   => $this->getSetValueOrNull($ldapUser, 'sn'),
+            'displayname' => $this->getSetValueOrNull($ldapUser, 'displayname'),
+            'emplid'      => $this->getSetValueOrNull($ldapUser, 'employeenumber'),
+            'studentid'   => $this->getSetValueOrNull($ldapUser, 'nustudentnumber')
+        ];
+    }
+}
